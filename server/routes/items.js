d430be43c630299bd397166264a543c1b1f0abc6
evanmrosas <evanmrosas@gmail.com>
2024-10-07 14:46:49 -0500

    added server routes for items in inventory

---
+++ server/routes/items.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const {Item} = require('../models')
+
+// GET all items
+router.get('/', async (req, res, next) => {
+    try{
+        const items = await Item.findAll()
+        res.json(items)
+    } catch(error){
+        next(error)
+    }
+})
+
+// GET one item
+router.get('/:id', async (req, res, next) => {
+    try{
+        const id = req.params.id
+        const item = Item.findByPk(id)
+        res.json(item)
+    } catch(error){
+        next(error)
+    }
+})
