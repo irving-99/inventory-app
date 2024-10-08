78f76978ba1579c187ef8de19c44cb534de349b9
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-10-08 16:55:36 -0500

    added in search by key term

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Item } from './Items';
 import SingleItem from '../components/SingleItem';
 
-export const ItemsList = ({setView,items, item, setItem, fetchSingleItem, deleteItem}) => {
+export const ItemsList = ({setView,items, item, setItem, fetchSingleItem, deleteItem, searchItem, setSearchItem, handleSearch}) => {
+
+
+
 	return <div className='items'>
 	<div>
+		<div className='search'>
+			<input placeholder='Search Items' value={searchItem} onChange={(e)=>setSearchItem(e.target.value)}/>
+			<button onClick={handleSearch}>Search</button>
+		</div>
 		<button onClick={()=> setView('addItem')}>Add Item</button>
 		<div className='item'>
 			{
